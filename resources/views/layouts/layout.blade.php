568d877cf937c8318c4d9d2a8ea84e76da29a17b
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2025-03-13 19:31:44 -0300

    feature: navigation

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title', 'Blog')</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+    @endif
+</head>
+
+<body class="bg-gray-900">
+
+    <header>
+        @yield('nav')
+    </header>
+
+    <main class="py-12">
+        <div class="container mx-auto">
+            @yield('content')
+        </div>
+    </main>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
